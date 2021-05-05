6aa5dca70824b912d946d84ca75626ef6b9c82b2
bakebitcourse@gmail.com <bakebitcourse@gmail.com>
2021-05-05 16:48:57 +0600

    first commit

---
+++ class1/index.php
@@ -0,0 +1,169 @@
+<?php 
+
+
+$nipa = array();
+
+$nipa = [
+            "Shahana"=>20,
+            "Shajjad"=>[
+                "BMW"=>6,
+                "Mar"=>10
+            ],
+            "Riza"=>[20,15,25]
+        ];
+
+        echo "<br>";
+echo $nipa["Riza"][1];
+
+echo "<br>";
+echo $nipa["Shajjad"]["Mar"];
+
+/* Operators */
+// $motein = array(); // array data type
+
+// $motein = ["Sony","Showrob","Istiaq",60,25,20]; //array
+
+// echo "<pre>";
+// var_dump($motein);
+// echo "</pre>";
+// echo "<br>";
+// echo "<br>";
+
+// echo "<pre>";
+// print_r($motein);
+// echo "</pre>";
+
+// $x = 40;
+// $y = 20;
+
+// $sum = $x + $y;
+// $div = $x / $y; //  Arithmetic operator
+
+// echo "<br>";
+// echo $_SERVER['HTTP_USER_AGENT'];
+
+// echo "<br>";
+
+// echo "Division of ".$x." and ".$y." is = ".$div;
+
+// echo "<br>";
+
+// $tanjil = 50; // Integer type
+
+// echo $tanjil." KG";
+// echo "<br>";
+// $lelena = 60.4; // Float data type;
+
+// echo $lelena;
+
+// echo "<br>";
+
+// $deen = Null;
+// echo "<br>";
+// echo $deen;
+
+// $joydip = false; // Boolean  0
+// echo "<br>";
+// echo $joydip;
+
+
+
+// $talha = "I am Talha";
+
+// $nur = "I am Nur";
+
+// $fatema = "I am Fatema";
+
+// $helal = "I am Helal";
+
+// echo "<br>";
+
+// echo $nur.$fatema;
+
+// echo "<br>";
+
+// echo $fatema;
+
+// echo "<br>";
+
+// echo $helal;
+
+// echo "<br>";
+
+
+// echo $talha;
+
+
+
+/*
+echo "<h1 style='color:blue'>Hello world</h1>";
+
+echo "I am ";
+// echo "<br>";
+echo "Saadman"; // String
+
+echo "<br>";
+
+echo "Saadman"." Sayeed"; // Concatanation
+
+echo "<br>";
+
+echo "Bangla"." "."Desh";
+
+echo "<br>";
+
+echo 10-5; // Integer
+
+
+echo "<br>";
+
+echo 10*5; // Integer
+
+
+echo "<br>";
+
+echo "50 divided by 2 is = " ;
+
+echo 50/2 ;
+
+echo "<br>";
+
+//echo ' I\'m Saad  ';
+
+echo "<br>";
+*/
+
+
+
+
+?>
+
+<!-- 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+ 
+<div class="container">
+
+    <h1>
+        Hello World
+    </h1>
+
+</div>
+
+
+
+
+</body>
+</html>
+ -->
